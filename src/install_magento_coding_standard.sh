6c302bf815ca3947329503ea67b981130810c627
Piotr Kwiecinski <piotr.kwiecinski@codemanufacture.com>
2023-06-04 19:29:22 +0200

    rework tests

--- src/install_magento_coding_standard.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -eou pipefail
-
-composer global require --no-interaction magento/magento-coding-standard:"${INPUT_MAGENTO2_STANDARD_VERSION:-*}"
-composer global require --no-interaction phpcompatibility/php-compatibility:*
-composer global -- exec phpcs --config-set installed_paths \
-         ../../magento/magento-coding-standard/,../../phpcompatibility/php-compatibility
